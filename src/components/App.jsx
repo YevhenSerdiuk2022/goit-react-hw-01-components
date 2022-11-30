de59aeb2ed624c97fb5567e959724b2e0a6959ba
YevhenSerdiuk2022 <99655492+YevhenSerdiuk2022@users.noreply.github.com>
2022-11-30 21:38:21 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import user from 'components/Profile/user.json';
+
+import { Profile } from 'components/Profile/Profile';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile/>
     </div>
   );
 };
